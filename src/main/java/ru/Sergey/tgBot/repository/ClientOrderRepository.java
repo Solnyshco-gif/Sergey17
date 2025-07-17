8305a1c139e0ad337d5a5478b33fce95c959aba2
Solnyshco-gif <shtyrbus@yandex.ru>
2025-07-17 20:27:06 +0300

    Практическая работа 4. REST API и сервисный слой.

--- src/main/java/ru/Sergey/tgBot/repository/ClientOrderRepository.java
+++ src/main/java/ru/Sergey/tgBot/repository/ClientOrderRepository.java
@@ -3,6 +3,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.Sergey.tgBot.entity.ClientOrder;
 
+import java.util.List;
+
 @RepositoryRestResource(collectionResourceRel = "client-orders", path = "client-orders")
 public interface ClientOrderRepository extends JpaRepository<ClientOrder, Long> {
+    List<ClientOrder> findByClientId(Long clientId);
 }
--- src/main/java/ru/Sergey/tgBot/repository/OrderProductRepository.java
+++ src/main/java/ru/Sergey/tgBot/repository/OrderProductRepository.java
@@ -1,9 +1,16 @@
 package ru.Sergey.tgBot.repository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.Sergey.tgBot.entity.OrderProduct;
+import ru.Sergey.tgBot.entity.Product;
+
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "order-products", path = "order-products")
 public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
+    List<Product> findTopPopularProducts(PageRequest of);
+
+    List<Product> findProductsByClientId(Long clientId);
 }
 
