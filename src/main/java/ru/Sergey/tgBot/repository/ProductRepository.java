e8613711c2961e0347d1fb11b63bce4ad0689762
Solnyshco-gif <shtyrbus@yandex.ru>
2025-07-17 20:27:06 +0300

    Практическая работа 4. REST API и сервисный слой.

--- src/main/java/ru/Sergey/tgBot/repository/ProductRepository.java
+++ src/main/java/ru/Sergey/tgBot/repository/ProductRepository.java
@@ -3,7 +3,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.Sergey.tgBot.entity.Product;
 
+import java.util.List;
+
 @RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByNameContainingIgnoreCase(String name);
+
+    List<Product> findByCategoryId(Long categoryId);
 }
 
